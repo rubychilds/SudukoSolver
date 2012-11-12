054b4c2f0d6029ee85944f3348f85cb3c5ea2484
rubychilds <rubychilds.live.co.uk>
2012-11-12 13:03:56 -0500

    corrected a few errors-ensure functionality board

--- sudukogrid.py
+++ sudukogrid.py
@@ -2,26 +2,26 @@ import pygame
 from pygame.locals import *
 from pygame import Color, Rect, Surface
 
-class board:
+class Board:
 
 	def __init__(self, screen):
 
 		screen_size = screen.get_size()
 
 		# deals with background
-		background = pygame.Surface(screen.get_size())
-		background = background.convert()
-		background.fill ((250,250,250))
+		self.background = pygame.Surface(screen.get_size())
+		self.background = self.background.convert()
+		self.background.fill ((250,250,250))
 
 		# Font 
 		font = pygame.font.SysFont('Subway Black', 30, bold = False, italic = False)
 	  	text = font.render("Suduko Solver", 1, (10, 10, 10)) # sets font type, size then rgb tuple
 	  	textpos = text.get_rect()
-	  	textpos.centerx = background.get_rect().centerx
-	  	background.blit(text, textpos)
+	  	textpos.centerx = self.background.get_rect().centerx
+	  	self.background.blit(text, textpos)
 
 		print screen_size
-		pygame.draw.rect(background, Color("Black"), Rect(30, 40,400,400))
+		pygame.draw.rect(self.background, Color("Black"), Rect(30, 40,400,400))
 
 		# Starting position of the rectangle
 		rect_x = 36
@@ -37,52 +37,63 @@ class board:
 
 		for x in range(1,10):
 			for y in range(1,10):
-				pygame.draw.rect(background, Color("white"), Rect(rect_x + rect_change_x, rect_y + rect_change_y, width, height))			
+				pygame.draw.rect(self.background, Color("white"), Rect(rect_x + rect_change_x, rect_y + rect_change_y, width, height))			
 				rect_change_y += 42 
 
 				if y % 3 == 0 :
 					rect_change_y +=2
-
 
 			rect_change_x += 42
 			rect_change_y = 0
 			if x % 3 == 0 :
 					rect_change_x +=2
 			#print rect_change_x
-		return background
+	
 
 		# function to set numbers on grid
-		def initNumbers(numbers):
+	#def initNumbers(numbers):
 
 		# add buttons to window
 
 		# mouse position
 
+	def getBackground(self):
+		return self.background
 
-		def showBoard(ttt, board):
-			ttt.blit(board,(0,0))
+	# takes its self as a parameter
+	def showBoard(self,ttt):
+			ttt.blit(self.background,(0,0))
 			pygame.display.flip()
 
-def main(numbers):
+# MAIN PROGRAMME 
+def main():
 	# Initialise screen
  	pygame.init() # calls font initialisation automatically otherwise we can use pygame.font.init()
  	screen = pygame.display.set_mode((580, 500))
  	pygame.display.set_caption('Basic Pygame program')
 
 	#  Displays board
-	board_inst = board(screen)
-
+	board_inst = Board(screen)
+	# for running of the game
 	running = 1
+	# 
+	mouse = false
+
+
 
 	# Event loop - infinite until event i.e. closing of window
   	while (running == 1):
   		for event in pygame.event.get():
   			if event.type is QUIT:
   	 			running = 0
-  	 	board_inst.showBoard(screen, board)
+  	 		elif event.type == MOUSEMOTION:
+  	 			mousex, mousey = event.pos
+  	 		elif event.type == MOUSEBUTTONUP:
+  	 			mousex, mousey = event.pos
+  	 			mouseclicked = true
 
 
-
+  	 	board_inst.showBoard(screen)
 
 if __name__ == "__main__":
 	main()
